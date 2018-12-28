5acbd375e3512ad6b54e196d1e3ad4aea10c4e22
juanico <juanjose.marin3@um.es>
2018-12-28 19:54:08 +0100

    Implemented basic functionality in "Hilo" and "Programa"

--- ejercicio2/src/main/Programa.java
+++ ejercicio2/src/main/Programa.java
@@ -1,9 +1,81 @@
 package main;
 
-public class Programa {
+import java.util.concurrent.Semaphore;
 
+import paneles.Panel;
+import threads.Hilo;
+
+/**
+ * 
+ * @author juanico
+ *
+ */
+public class Programa {
+	
+	/*-----------------------------------------
+	 * _______________CONSTANTS_______________
+	 ----------------------------------------*/
+	/**
+	 * 
+	 */
+	public static final int CANT_THREADS = 10;
+	
+	/**
+	 * 
+	 */
+	public static final int DIM_MATRIX = 3;
+	
+	/*-----------------------------------------
+	 * ___________GLOBAL VARIABLES____________
+	 ----------------------------------------*/
+	/**
+	 * 
+	 */
+	public static Semaphore mutex = new Semaphore(1);
+	
+	/**
+	 * 
+	 */
+	public static Semaphore panelesLibres = new Semaphore(DIM_MATRIX);
+	
+	/**
+	 * 
+	 */
+	public static Panel[] paneles = new Panel[DIM_MATRIX];
+	
+	/**
+	 * 
+	 */
+	public static boolean[] isPanelLibre = new boolean[DIM_MATRIX];
+	
+	private static Thread[] hilos = new Thread[CANT_THREADS];
+	
+	/*-----------------------------------------
+	 * _________PROGRAMA PRINCIPAL____________
+	 ----------------------------------------*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Inicialmente todos los paneles libres
+		for (int i = 0; i < DIM_MATRIX; i++) {
+			isPanelLibre[i] = true;
+			paneles[i] = new Panel("Panel Nº " + (i + 1), 20+450*i, 100);
+		}
+		
+		for (int i = 0; i < CANT_THREADS; i++) {
+			hilos[i] = new Hilo(i + 1); 
+		}
+		
+		for (Thread threads : hilos) {
+			threads.start();
+		}
+		
+		for (Thread threads : hilos) {
+			try {
+				threads.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
--- ejercicio2/src/threads/Hilo.java
+++ ejercicio2/src/threads/Hilo.java
@@ -1,5 +1,105 @@
 package threads;
 
+import main.Matrix;
+import paneles.Panel;
+
+import static main.Programa.*;
+
+/**
+ * 
+ * @author juanico
+ *
+ */
 public class Hilo extends Thread {
 
+	private final int identificator;
+	private Matrix Ma, Mb, Mresult;
+	
+	/**
+	 * 
+	 * @param identificator
+	 */
+	public Hilo(int identificator) {
+		this.identificator = identificator;
+		this.Ma = new Matrix(CANT_THREADS, CANT_THREADS);
+		this.Mb = new Matrix(CANT_THREADS, CANT_THREADS);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int getIdentificator() {
+		return identificator;
+	}
+	
+	/**
+	 * run-> Pone en marcha el hilo.
+	 * CONDICIONES DE SINCRONIZACION
+	 * -Que los hilos no usen el mismo panel al mismo tiempo
+	 * 
+	 * EXLUSION MUTUA:
+	 * La exlusion mutua de la pantalla se comprueba poniendo a false la posicion correspondiente del array de
+	 * booleanos. De esta forma otro que venga por detras no va a coger el mismo panel.
+	 * La exlusion mutua del array de booleanos se garantiza utilizando un semaforo mutex binario que 
+	 * se bloquee cuando eestamos accediendo a el. De esta forma cuando dos hilos entran al array al buffer 
+	 * al mismo tiempo evitamos que se produzca una situacion en la que dos hilos puedan acceder a la seccion
+	 * critica al mismo tiempo.
+	 * 
+	 */
+	@Override
+	public void run() {
+		
+		for (int i = 0; i < DIM_MATRIX; i++) {
+			int aux = 0;
+			
+			Ma.newRandomMatrix();
+			Mb.newRandomMatrix();
+			Mresult = Ma.sumMatrix(Mb);
+			
+			try {
+				panelesLibres.acquire();
+				mutex.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			/*
+			try {
+				mutex.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}*/
+			while(isPanelLibre[aux] == false) aux++;
+			
+			Panel panel = paneles[aux];
+			isPanelLibre[aux] = false;
+			
+			mutex.release();
+			
+			panel.escribir_mensaje("Usando panel el hilo " + identificator);
+			panel.escribir_mensaje("Matrix A = " + Ma.printMatrix());
+			panel.escribir_mensaje("Matrix B = " + Mb.printMatrix());
+			panel.escribir_mensaje("Matrix C = " + Mresult.printMatrix());
+			panel.escribir_mensaje("Terminando de usar panel el hilo " + identificator);
+			
+			try {
+				mutex.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			isPanelLibre[aux] = true;
+			
+			mutex.release();
+			panelesLibres.release();
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
 }
