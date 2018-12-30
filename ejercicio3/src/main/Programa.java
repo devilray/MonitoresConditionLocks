b74e7c14be159977cbbdfe04d9d4d419d967d836
juanico <juanjose.marin3@um.es>
2018-12-31 00:53:54 +0100

    Created basic structure of exercise 3.

---
+++ ejercicio3/src/main/Programa.java
@@ -0,0 +1,31 @@
+package main;
+
+/**
+ * 
+ * Desarrollar un programa concurrente en Java para resolver el problema siguiente: 
+ * Supongamos que hay 30 procesos que tras estar un tiempo en ejecución (representado como 
+ * una llamada a Thread.sleep(tiempoAleatorio)) generan aleatoriamente un número del conjunto 
+ * {1,2,3,4}. Tras continuar durante un tiempo su ejecución (representado como otra llamada a 
+ * Thread.sleep(tiempoAleatorio)) cada proceso debe imprimir la cantidad de procesos en el 
+ * sistema que tienen su mismo número (incluyéndose él mismo) sabiendo todos que hay 30 
+ * procesos en ejecución. La impresión debe ser:
+
+Hilo id
+Valor asignado = 
+Hilos con el mismo valor = 
+
+La sincronización hay que resolverla con monitores de la forma que consideres más
+eficiente.
+
+ * @author Juan José Marín Peralta
+ * @version 3.0
+ */
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
---
+++ ejercicio3/src/main/SharedMemory.java
@@ -0,0 +1,28 @@
+package main;
+
+import monitor.Monitor;
+
+public class SharedMemory {
+
+	//************************************************************************
+	//************************ CONSTANTES NUMÉRICAS **************************
+	//************************************************************************	
+	/**
+	 * Constante numérica que representa el número de procesos que se van a ejecutar
+	 */
+	public static final int NUMERO_PROCESOS = 30;			
+	
+	//************************************************************************
+	//************************* VARIABLES GLOBALES ***************************
+	//************************************************************************	
+	/**
+	 * Variable global que representa el conjunto de números aleatorios que vamos a generar entre 1 y 4, ambos inclusive y que se obtienen en cada hilo.
+	 */
+	public static int[] cjto = new int [NUMERO_PROCESOS]; 
+	
+	/**
+	 * Variable global que representa el monitor que va a controlar el acceso a la sección crítica.
+	 */
+	public static Monitor monitor = new Monitor();
+	
+}
