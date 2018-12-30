35d925eedb08a65b10d241293b3346eb957468bf
juanico <juanjose.marin3@um.es>
2018-12-31 00:53:38 +0100

    Added constructor to exercise 1.

--- ejercicio1/src/threads/HiloMultiplicador.java
+++ ejercicio1/src/threads/HiloMultiplicador.java
@@ -4,6 +4,10 @@ import static main.SharedMemory.*;
 
 public class HiloMultiplicador implements Runnable {
 
+	public HiloMultiplicador() {
+		
+	}
+	
 	@Override
 	public void run() {
 		
--- ejercicio1/src/threads/HiloSumador.java
+++ ejercicio1/src/threads/HiloSumador.java
@@ -4,6 +4,10 @@ import static main.SharedMemory.*;
 
 public class HiloSumador implements Runnable {
 
+	public HiloSumador() {
+		
+	}
+	
 	@Override
 	public void run() {
 		
