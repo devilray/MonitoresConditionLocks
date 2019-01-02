a99d25fc24233e4b28b8267d045c7f0f7a35f7ed
juanico <juanjose.marin3@um.es>
2019-01-02 16:32:06 +0100

    Creado JavaDoc

--- ejercicio2/src/main/Matrix.java
+++ ejercicio2/src/main/Matrix.java
@@ -3,9 +3,9 @@ package main;
 import java.util.Random;
 
 /**
- * 
+ * Clase que representa el tipo de una Matriz con una anchura y una altura que se construyen al comienzo.
  * @author juanico
- *
+ * @version 3.0
  */
 public class Matrix {
 
@@ -13,9 +13,9 @@ public class Matrix {
 	private int[][] values;
 	
 	/**
-	 * 
-	 * @param ancho
-	 * @param alto
+	 * Constuye una matriz de anchura y altura (Ancho X Alto) establecidos en los parámetros del constructor.
+	 * @param ancho Anchura que tendrá la matriz. Número de Filas.
+	 * @param alto Altura que tendrá la matriz. Número de Columnas.
 	 */
 	public Matrix(int ancho, int alto) {
 		this.ancho = ancho;
@@ -24,23 +24,23 @@ public class Matrix {
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Obtiene la anchura de la matriz
+	 * @return anchura de la matriz.
 	 */
 	public int getAncho() {
 		return ancho;
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Obtiene la altura de la matriz.
+	 * @return altura de la matriz.
 	 */
 	public int getAlto() {
 		return alto;
 	}
 	
 	/**
-	 * 
+	 * Genera una nueva matriz con valores aleatorios de anchura x altura establecidos en la construcción.
 	 */
 	public void newRandomMatrix() {
 		for (int i = 0; i < ancho; i++) {
@@ -51,9 +51,9 @@ public class Matrix {
 	}
 	
 	/**
-	 * 
-	 * @param m
-	 * @return
+	 * Método que realiza la suma de dos matrices. La primera matriz es la instancia actual (el objeto recetor de la llamada) y la segunda matriz es la que se le pasa por parámetro. 
+	 * @param m Matriz que se le sumará al objeto receptor que será otra matriz del mismo tipo.
+	 * @return Matriz que resulta la suma de la matriz de la instancia actual y la matriz pasada por parámetro.
 	 */
 	public Matrix sumMatrix(Matrix m) {
 		Matrix result = new Matrix(ancho, alto);
@@ -68,8 +68,8 @@ public class Matrix {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Método que imprime la matriz con formato propio de una matriz en filas y columnas.
+	 * @return cadena de caracteres que representa a una matriz.
 	 */
 	public String printMatrix() {
 		String result = new String();
